cbb35b450ad51c2e014b6e2261ad14559177b415
jesapelgrub <jesapelgrub@gmail.com>
2024-12-17 18:49:38 +0100

    init project MVC with lombok and DB connexion

---
+++ src/main/java/fr/fms/SpringShopJpaApplication.java
@@ -0,0 +1,93 @@
+/**
+ * Application console de simulation de l'administration de gestion d'un stock d'articles avec un menu permettant d'ajouter, supprimer, mettre à jour un article ou les afficher par pagination ou pas
+ * De même pour les catégories d'articles : ajouter, supprimer, maj ou afficher les catégories ou tous les articles d'une catégorie
+ * 
+ * @author El-Babili - 2023
+ */
+
+package fr.fms;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import fr.fms.dao.ArticleRepository;
+import fr.fms.dao.CategoryRepository;
+import fr.fms.entities.Article;
+import fr.fms.entities.Category;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootApplication
+public class SpringShopJpaApplication implements CommandLineRunner {
+	@Autowired
+	ArticleRepository articleRepository;
+	@Autowired
+	CategoryRepository categoryRepository;
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShopJpaApplication.class, args);
+	}
+
+
+	@Override
+	@Transactional
+	public void run(String... args) throws Exception {
+//		generateDatas();
+		List<Article> articles = articleRepository.findAll();
+		articles.forEach(article -> System.out.println(article));
+	}
+
+	/**
+	 * Méthode qui permet de regénérer les données dans la base
+	 * Il faudra basculer en mode create ou supprimer la bdd
+	 */
+	private void generateDatas() {		
+		Category smartphone = categoryRepository.save(new Category(0, "Smartphone","Tel mobile", null));
+		Category pc = categoryRepository.save(new Category(0,"Ordinateur","PC & Laptop",null));
+		Category tablet = categoryRepository.save(new Category(0,"Tablette"," ",null));
+		Category printer = categoryRepository.save(new Category(0, "Imprimante"," ",null));
+		Category camera = categoryRepository.save(new Category(0, "Camera", " ", null));
+		Category tv = categoryRepository.save(new Category(0, "TV"," ", null));
+		Category telescope = categoryRepository.save(new Category(0, "Telescope"," ",null));
+		Category gps = categoryRepository.save(new Category(0,"Gps"," ", null));
+		Category enceinte = categoryRepository.save(new Category(0, "Enceinte"," ",null));
+
+		articleRepository.save(new Article(0,"Samsung","S8",250,smartphone));
+		articleRepository.save(new Article(0,"Samsung","S9", 300,smartphone));
+		articleRepository.save(new Article(0,"Iphone","10",500,smartphone));
+		articleRepository.save(new Article(0,"Xiaomi","MI11",100,smartphone));
+		articleRepository.save(new Article(0,"OnePlus","9 Pro",200,smartphone));
+		articleRepository.save(new Article(0,"Google","Pixel 5",350,smartphone));
+		articleRepository.save(new Article(0,"Poco","F3",150,smartphone));
+
+		articleRepository.save(new Article(0,"Dell","810",550,pc));
+		articleRepository.save(new Article(0,"Asus","F756",600,pc));
+		articleRepository.save(new Article(0,"Asus","E80",700,pc));
+		articleRepository.save(new Article(0,"MacBook","Pro",1000,pc));
+		articleRepository.save(new Article(0,"MacBook","Air",1200,pc));
+
+		articleRepository.save(new Article(0,"IPad","XL 5",300,tablet));
+		articleRepository.save(new Article(0,"IPad","XL 7",500,tablet));
+
+		articleRepository.save(new Article(0,"Canon","MG30",50,printer));
+		articleRepository.save(new Article(0,"Canon","MG50",60,printer));
+		articleRepository.save(new Article(0,"HP","800",50,printer));
+		articleRepository.save(new Article(0,"Epson","3T",100,printer));
+
+		articleRepository.save(new Article(0,"GoPro","7",150,camera));
+		articleRepository.save(new Article(0,"GoPro","10",200,camera));
+
+		articleRepository.save(new Article(0,"Panasonic","HT",1500,tv));
+		articleRepository.save(new Article(0,"Philips","L43",450,tv));
+	}
+
+}
---
+++ src/main/java/fr/fms/dao/ArticleRepository.java
@@ -0,0 +1,35 @@
+package fr.fms.dao;
+
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import fr.fms.entities.Article;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+	public List<Article> findByBrand(String brand);
+	public List<Article> findByBrandContains(String brand);
+	public List<Article> findByBrandAndPrice(String brand, double price);
+	public List<Article> findByBrandContainsAndPriceGreaterThan(String brand, double price);
+	
+	@Query("select A from Article A where A.brand like %:x% and A.price > :y")
+	public List<Article> searchArticles(@Param("x")String brand,@Param("y")double price);
+	
+	@Query("select A from Article A where A.id= :id")
+	public Article findOne(@Param("id")long id);
+	
+	@Transactional
+	@Modifying
+	@Query("update Article a set a.brand= :b, a.price= :p where a.id= :id")
+	public int update(@Param("b")String brand ,@Param("p")double price , @Param("id")long id);
+	
+	public List<Article> findByDescriptionAndBrand(String description, String brand);
+	//public Page<Article> findAll(Pageable pageable);
+
+
+}
