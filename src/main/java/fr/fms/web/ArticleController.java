8df0d8eb34600135d4a2025557e6eb1a534d6090
jesapelgrub <jesapelgrub@gmail.com>
2024-12-18 09:05:12 +0100

    pagination

--- src/main/java/fr/fms/web/ArticleController.java
+++ src/main/java/fr/fms/web/ArticleController.java
@@ -3,13 +3,17 @@ package fr.fms.web;
 import fr.fms.dao.ArticleRepository;
 import fr.fms.entities.Article;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class ArticleController {
@@ -17,11 +21,14 @@ public class ArticleController {
     ArticleRepository articleRepository;
 
 
-    @RequestMapping("/index")
-    public String index (Model model) {
-          List<Article> articles = articleRepository.findAll();
-          model.addAttribute("listArticles", articles);
+    @GetMapping("/index")
+    public String index(Model model, @RequestParam(name = "page", defaultValue = "0") int page) {
+        Page<Article> articles = articleRepository.findAll(PageRequest.of(page, 5));
+        model.addAttribute("listArticles", articles.getContent());
+        model.addAttribute("page", IntStream.range(0, articles.getTotalPages()).boxed().collect(Collectors.toList()));
+        model.addAttribute("currentPage", page);
         return "articles";
     }
+
 
 }
