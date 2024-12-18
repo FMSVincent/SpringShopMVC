a345eae1044c1acaf52097757537e82681c68565
jesapelgrub <jesapelgrub@gmail.com>
2024-12-18 20:36:56 +0100

    display category name in table

--- src/main/java/fr/fms/web/ArticleController.java
+++ src/main/java/fr/fms/web/ArticleController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -72,7 +71,6 @@ public class ArticleController {
         model.addAttribute("categoryId", categoryId);
         return "articles";
     }
-
 
 
 }
