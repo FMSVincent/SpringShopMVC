3a7beb010b18f65ffcdbe138e6fffe0a093e2e77
jesapelgrub <jesapelgrub@gmail.com>
2025-01-10 06:37:11 +0100

    test get all article

--- src/test/java/fr/fms/SpringShopJpaTests.java
+++ src/test/java/fr/fms/SpringShopJpaTests.java
@@ -12,7 +12,10 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -33,4 +36,17 @@ public class SpringShopJpaTests {
         assertEquals("anonymous", article.getBrand());
     }
 
+    @Test
+    void test_get_all_articles() {
+        int initialSize = articleRepository.findAll().size();
+
+        Category category1 = categoryRepository.save(new Category(0, "category1", "description1", null));
+        Category category2 = categoryRepository.save(new Category(0, "category2", "description2", null));
+
+        articleRepository.save(new Article(0, "brand1", "description1", 100.0, category1));
+        articleRepository.save(new Article(0, "brand2", "description2", 200.0, category2));
+
+        int newSize = articleRepository.findAll().size();
+        assertEquals(initialSize + 2, newSize);
+    }
 }
